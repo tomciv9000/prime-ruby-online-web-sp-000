b33804a77ff1b239456689a26dac50521df5f635
Tom White <eugene.white@gmail.com>
2019-05-23 14:37:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,13 @@
-def prime?(num)
-  i = 2 
-  while i < num
-    return false if num % n == 0
+def prime?(int)
+  if int <= 1 
+    return false 
+  end
+  i = 2
+  while i < int 
+    if int % i == 0 
+      return false
+    end
+    i += 1
+  end
+  return true
 end
