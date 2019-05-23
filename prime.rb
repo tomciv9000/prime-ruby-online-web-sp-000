fcdf2c3d11e4e7bdd5e7e005fa157f0a56cbaebb
Tom White <eugene.white@gmail.com>
2019-05-23 15:08:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,3 +2,15 @@ def prime?(int)
   return false if int <=1 
   (2..Math.sqrt(int)).none?{|i| int % i==0}
 end
+
+def original_prime?(int)
+  return false if int <= 1 
+  i = 2
+  while i < int 
+    if int % i == 0 
+      return false
+    end
+    i += 1
+  end
+  return true
+end
