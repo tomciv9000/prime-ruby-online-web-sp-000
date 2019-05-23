c0bbce853f96f366de5fefe95b5ed10d5a2ee2c7
Tom White <eugene.white@gmail.com>
2019-05-23 15:06:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,4 @@
-def dodoprime?(int)
-  return false if int <= 1 
-  i = 2
-  while i < int 
-    if int % i == 0 
-      return false
-    end
-    i += 1
-  end
-  return true
-end
+
 
 def prime?(int)
   return false if int <=1 
